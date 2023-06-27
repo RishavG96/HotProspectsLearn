1b3bc5ba53b4d20257763aaf153e8439ce2d77fe
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-27 07:40:03 +0530

    Initial Commit

---
+++ HotProspectsLearn/HotProspectsLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsLearnApp.swift
+//  HotProspectsLearn
+//
+//  Created by Rishav Gupta on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
