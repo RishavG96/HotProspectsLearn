136604c7404198a5e528a8f95ea5aeea1b0c5f39
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-27 08:27:55 +0530

    Day 79 done

--- HotProspectsLearn/ContentView.swift
+++ HotProspectsLearn/ContentView.swift
@@ -6,16 +6,63 @@
 //
 
 import SwiftUI
+// @EnvironmentObject - place an object into the environment so that any child view can use it and have access to it and have updates to it as well
+
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
 
 struct ContentView: View {
+    @StateObject var user = User()
+    
+    
+    @State private var selectedTab = "One"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        VStack {
+////            EditView().environmentObject(user)
+////            DisplayView().environmentObject(user)
+//            EditView()
+//            DisplayView()
+//        }
+//        .environmentObject(user)
+        
+        // Environment uses Data types for the keys  and instance of that type as value
+        // [User: instance of User]
+        
+        
+        // TabView Contains the Navigation View which contains List
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
         }
-        .padding()
     }
 }
 
